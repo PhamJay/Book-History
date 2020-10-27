ded33389d853a28b53091a774560b8655eac494a
Pham Van-Jay <Van-Jay.Pham@twinformatics.at>
2020-10-27 11:43:14 +0100

    Correct displaying of historic data

--- book-history/src/app/app.component.ts
+++ book-history/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { BookDatasource } from './book-datasource.service';
 import { BookService } from './book.service';
 import { BookChange } from './book-change'
 @Component({
@@ -10,13 +9,55 @@ import { BookChange } from './book-change'
 export class AppComponent implements OnInit{
   title = 'Book History';
   displayedColumns: string[] = ['timestamp', 'id', 'revision', 'description'];
-  bookChanges = null;
+  bookChanges = new Array<BookChange>();
+  dataLoaded = false;
 
   constructor(private bookService: BookService) {
   }
 
   ngOnInit(): void {
-    this.bookChanges = this.bookService.getBookChanges()
-    console.log('last')
+    this.fetchData();
+  }
+
+  async fetchData(){
+    let books = await this.bookService.getBookChanges();
+    console.log(books)
+    this.getHistory(books);
+  }
+
+  getHistory(books){
+    for (const book of books) {
+      const bookChange = new BookChange();
+      bookChange.timestamp = book.timestamp;
+      bookChange.id = book.id
+      bookChange.revision = book.revision
+      switch(book.action){
+        case 'insert':
+          bookChange.description = 'Book has been INSERTED into the database';
+          break;
+        case 'delete':
+          bookChange.description = 'Book has been DELETED from the database';
+          break;
+        case 'update':
+          bookChange.description = 'Book has been UPDATED. '
+
+          const previousRevision = books.find(previous => (previous.id === book.id) && (previous.revision === (book.revision - 1)))
+
+          if(book.title != previousRevision.title){
+            bookChange.description += 'TITLE has been changed from "' + previousRevision.title + '" to "' + book.title + '". ';
+          }if(book.description != previousRevision.description){
+            bookChange.description += 'DESCRIPTION has been changed from "' + previousRevision.description + '" to ' + book.description + '". '
+          }if(book.authors != previousRevision.authors){
+            bookChange.description += 'AUTHORS have been changed from "' + previousRevision.authors + '" to ' + book.authors + '". '
+          }if(book.publish_date != previousRevision.publish_date){
+            bookChange.description += 'PUBLISH DATE has been changed from "' + previousRevision.publish_date + '" to ' + book.publish_date + '". '
+          }
+      }
+      this.bookChanges.push(bookChange);
+      console.log('push')
+    }
+
+    console.log(this.bookChanges)
+    this.dataLoaded = true;
   }
 }
--- book-history/src/app/book.service.ts
+++ book-history/src/app/book.service.ts
@@ -15,47 +15,7 @@ export class BookService {
 
   constructor(private http: HttpClient) { }
 
-  getBookChanges(): BookChange[] {
-    const bookChanges = new Array<BookChange>();
-
-    console.log('1');
-
-    this.http.get<Book[]>(this.serviceUrl).pipe(map(result => {
-      console.log("call finished");
-      for (const book of result) {
-        const bookChange = new BookChange();
-        bookChange.timestamp = book.timestamp;
-        bookChange.id = book.id
-        bookChange.revision = book.revision
-        switch(book.action){
-          case 'insert':
-            bookChange.description = 'Book has been INSERTED into the database';
-            break;
-          case 'delete':
-            bookChange.description = 'Book has been DELETED from the database';
-            break;
-          case 'update':
-            bookChange.description = 'Book has been UPDATED. '
-  
-            const previousRevision = result.find(previous => (previous.id === book.id) && (previous.revision === (book.revision - 1)))
-  
-            if(book.title != previousRevision.title){
-              bookChange.description += 'TITLE has been changed from "' + previousRevision.title + '" to "' + book.title + '". ';
-            }if(book.description != previousRevision.description){
-              bookChange.description += 'DESCRIPTION has been changed from "' + previousRevision.description + '" to ' + book.description + '". '
-            }if(book.authors != previousRevision.authors){
-              bookChange.description += 'AUTHORS have been changed from "' + previousRevision.authors + '" to ' + book.authors + '". '
-            }if(book.publish_date != previousRevision.publish_date){
-              bookChange.description += 'PUBLISH DATE has been changed from "' + previousRevision.publish_date + '" to ' + book.publish_date + '". '
-            }
-        }
-  
-        bookChanges.push(bookChange);
-        console.log('push')
-      }
-    })).subscribe()
-    
-    setTimeout(() => console.log(bookChanges), 1500);
-    return bookChanges;
+  async getBookChanges() {
+    return await this.http.get<Book[]>(this.serviceUrl).toPromise();
   }
 }
